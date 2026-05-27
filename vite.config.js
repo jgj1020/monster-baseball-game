8bc2485c8ded61175bdbf762d418f0faf922f393
jgj1020 <s2433@e-mirim.hs.kr>
2026-05-27 10:46:10 +0900

    이미지 추가 및 경로 최종 수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/monster-baseball-game/',
+  // 깃허브 페이지에 배포할 때 절대 경로 오류(404)를 막아주는 필수 설정입니다!
+  base: '/monster-baseball-game/', 
 })
